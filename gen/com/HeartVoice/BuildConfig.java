1f6fd4ca5d9bd3e7d061ce6d60242e811824433d
Wang Yan <wangyan87@gmail.com>
2013-03-17 18:17:39 +0800

    initial project

---
+++ gen/com/HeartVoice/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.HeartVoice;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/HeartVoice/MyActivity.java
@@ -0,0 +1,15 @@
+package com.HeartVoice;
+
+import android.os.Bundle;
+import org.apache.cordova.*;
+
+public class MyActivity extends DroidGap
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
