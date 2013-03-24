c9434e98668ef89d1710840ff7704f2838e692c5
Ying Fu <saharaying@gmail.com>
2013-03-24 13:17:11 +0800

    #3 basic panel layout

--- assets/www/js/main.js
+++ assets/www/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     document.addEventListener("deviceready", onDeviceReady, false);
+//    onDeviceReady();
 });
 
 function onDeviceReady(){
-    $("#init_screen").hide();
-    $("#main_screen").show();
+    initCommonSentences();
 }
 
 function startRecognizer(){
@@ -37,4 +37,36 @@ function onResults(response) {
     console.log(recognizerResult.text + "##" + recognizerResult.confidence);
     $("#text").append(recognizerResult.text + "##");
   })
+}
+
+function initCommonSentences() {
+  var commonSentences = {
+    '旅行': [
+        '请问哪里有旅馆?',
+        '请问我该怎么去火车站？'
+    ],
+    '生活': [
+        '附近哪里有饭馆？'
+    ]
+  }
+
+  var categories = $('.categories');
+
+  var count = 0;
+  for (var category in commonSentences) {
+    var id = 'category_' + count;
+    count++;
+    var categoryElem = $("<a href='#" + id + "' class='button'>" + category + "</a>");
+    categories.append(categoryElem);
+
+    var categorySentencesElem = $("<div class='panel'><ul></ul></div>");
+    categorySentencesElem.attr('id', id);
+    categorySentencesElem.attr('title', category);
+
+    var sentenceElems = $.each(commonSentences[category], function(index, sentence) {
+      categorySentencesElem.find('ul').append("<li>" + sentence + "</li>");
+    });
+    categorySentencesElem.appendTo($('#content'));
+  }
+
 }
