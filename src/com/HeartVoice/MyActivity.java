1cc2114158f9c2932882d4fb9330fd7908137017
Wang Yan <wangyan87@gmail.com>
2013-04-08 00:44:34 +0800

    enable detect sensor orientation

--- src/com/HeartVoice/MyActivity.java
+++ src/com/HeartVoice/MyActivity.java
@@ -1,16 +1,12 @@
 package com.HeartVoice;
 
-import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.OrientationEventListener;
 import org.apache.cordova.*;
 
 public class MyActivity extends DroidGap {
     /**
      * Called when the activity is first created.
      */
-    OrientationEventListener myOrientationEventListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
--- src/com/HeartVoice/OrientationListener.java
+++ src/com/HeartVoice/OrientationListener.java
@@ -10,16 +10,32 @@ public class OrientationListener extends OrientationEventListener {
 
 
     private CordovaWebView webView;
-    private CallbackContext callbackContext;
+    public static final int THRESHOLD = 15;
+    public String currentOrientation = "Portrait";
 
     public OrientationListener(Context context, int rate, CordovaWebView webView, CallbackContext callbackContext) {
         super(context, rate);
         this.webView = webView;
-        this.callbackContext = callbackContext;
     }
 
     @Override
-    public void onOrientationChanged(int orientation) {
-        webView.loadUrl("javascript:alert('hello world');");
+    public void onOrientationChanged(int angle) {
+        if (isChangedOrientation(angle, 90, 270, "Portrait")){
+            Log.d("Orientation Changed:", "From Portrait to Landscape");
+            webView.loadUrl("javascript:alert('From Portrait to Landscape')");
+        } else if(isChangedOrientation(angle, 0, 360, "Portrait")) {
+            Log.d("Orientation Changed:", "From Landscape to Portrait");
+            webView.loadUrl("javascript:alert('From Landscape to Portrait')");
+        }
+    }
+
+    private boolean isChangedOrientation(int angle, int first, int last, String orientation) {
+       if (angle >= (first - THRESHOLD) && angle <= (first + THRESHOLD) || angle >= (last - THRESHOLD) && angle <= (last + THRESHOLD)) {
+          if (!currentOrientation.equals(orientation)){
+              currentOrientation = orientation;
+              return true;
+          }
+       }
+       return false;
     }
 }
