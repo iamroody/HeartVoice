9d12370a61364db95d40b2db5a173e2e336b347a
Wang Yan <wangyan87@gmail.com>
2013-04-17 11:37:16 +0800

    auto swap slides

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,14 @@ $(function(){
       	height: 800,
         navigation: {
         	active: false
-        }
+        },
+	play: {
+      		effect: "slide",
+      		interval: 4000,
+      		auto: true,
+      		swap: true,
+      		pauseOnHover: false,
+      		restartDelay: 2500
+    	}
       });
     });
